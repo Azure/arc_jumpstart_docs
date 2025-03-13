f95b54a61f157f25cc033c772925b0ffe20cb6b1
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2025-03-13 10:56:54 +0200

    Fix sourcePath reference in Update-FrontMatter call in copy-JumpstartDocs.ps1

--- scripts/powershell/copy-JumpstartDocs.ps1
+++ scripts/powershell/copy-JumpstartDocs.ps1
@@ -107,7 +107,7 @@ try {
             #}
 
             # Update content with modified source path and write to destination
-            $updatedContent = Update-FrontMatter -content $content -sourcePath $using:sourcePath
+            $updatedContent = Update-FrontMatter -content $content -sourcePath $using:RelativePath
             Set-Content -Path $targetPath -Value $updatedContent -Encoding UTF8 -Force
         }
 
